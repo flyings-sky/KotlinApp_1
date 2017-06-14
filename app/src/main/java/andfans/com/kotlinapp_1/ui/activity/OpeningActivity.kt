e771446b728b3df0b7c9dcbef47f56313f1e5668
flyings-sky <614015229@qq.com>
2017-06-14 17:18:46 +0800

    增加了部分动画效果

--- app/src/main/java/andfans/com/kotlinapp_1/ui/activity/OpeningActivity.kt
+++ app/src/main/java/andfans/com/kotlinapp_1/ui/activity/OpeningActivity.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.*
 import com.yasic.library.particletextview.MovingStrategy.RandomMovingStrategy
 import com.yasic.library.particletextview.Object.ParticleTextViewConfig
 import kotlinx.android.synthetic.main.activity_opening.*
@@ -16,15 +17,20 @@ import top.wefor.circularanim.CircularAnim
 
 class OpeningActivity : AppCompatActivity() {
     var flag = false
-    lateinit var mContext:Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_opening)
-        mContext = this
         init()
     }
 
     private fun init(){
+        val rotateAnimation = RotateAnimation(0f,360f,Animation.RELATIVE_TO_SELF,
+                0.5f,Animation.RELATIVE_TO_SELF,0.5f)
+        val alphaAnimation = AlphaAnimation(1.0f,0.1f)
+        val animationSet = AnimationSet(true)
+        animationSet.addAnimation(rotateAnimation)
+        animationSet.addAnimation(alphaAnimation)
+        animationSet.duration = 1000
         val randomMovingStrategy = RandomMovingStrategy()
         val config = ParticleTextViewConfig.Builder()
                 .setRowStep(8)
@@ -48,15 +54,31 @@ class OpeningActivity : AppCompatActivity() {
                 }
             }
             if(flag){
-                Thread.sleep(1100)
+                Thread.sleep(1000)
                 uiThread {
                     particleTextView.stopAnimation()
                     start.visibility = View.VISIBLE
                 }
             }
         }
+
         start.setOnClickListener {
-                startActivitys()
+                start.animation = animationSet
+                start.startAnimation(animationSet)
+                animationSet.setAnimationListener(object : Animation.AnimationListener{
+                    override fun onAnimationEnd(animation: Animation?) {
+                        start.setImageResource(R.drawable.start_end)
+                        startActivitys()
+                    }
+
+                    override fun onAnimationRepeat(animation: Animation?) {
+
+                    }
+
+                    override fun onAnimationStart(animation: Animation?) {
+
+                    }
+                })
         }
 
     }
